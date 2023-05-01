d5b91c0e29524b068d4cd7ea3a50e26105a99966
magnunm <magnusnym97@gmail.com>
2023-05-01 14:24:07 +0200

    Refactor main function

--- rsa_key_converter/app.py
+++ rsa_key_converter/app.py
@@ -1,14 +1,8 @@
-"""Convert between RSA private and public key formats.
-
-Shorthand names for RSA numbers:
-n = The public modulus
-e = The public exponent
-p = One of the primes composing n
-q = The other of the primes composing n
-d = The private exponent
-
-"""
+"""Convert between RSA private and public key formats."""
+from __future__ import annotations
+from typing import Union, Optional
 from argparse import ArgumentParser
+from dataclasses import dataclass
 
 from .formats import (
     PrivateKeyFormat,
@@ -20,19 +14,55 @@ from .formats import (
 )
 
 
-PRIVATE_KEY_FORMATS: list[type[PrivateKeyFormat]] = [JWKPrivateKey, PEMPrivateKey]
-PUBLIC_KEY_FORMATS: list[type[PublicKeyFormat]] = [JWKPublicKey, PEMPublicKey]
+PRIVATE_KEY_FORMATS: dict[str, type[PrivateKeyFormat]] = {
+    f.name(): f for f in [JWKPrivateKey, PEMPrivateKey]
+}
+PUBLIC_KEY_FORMATS: dict[str, type[PublicKeyFormat]] = {
+    f.name(): f for f in [JWKPublicKey, PEMPublicKey]
+}
 
 
 def main():
-    private_key_formats = {f.name(): f for f in PRIVATE_KEY_FORMATS}
-    public_key_formats = {f.name(): f for f in PUBLIC_KEY_FORMATS}
+    args = parse_args()
+    if not args:
+        return
 
+    try:
+        print(args.input_output_formats.convert_key(args.input))
+    except ValueError as e:
+        print(str(e))
+
+
+@dataclass
+class Args:
+    input: str
+    input_output_formats: Union[InputOutputFormatsPrivate, InputOutputFormatsPublic]
+
+
+@dataclass
+class InputOutputFormatsPublic:
+    input_format: type[PublicKeyFormat]
+    output_format: type[PublicKeyFormat]
+
+    def convert_key(self, input_key: str) -> str:
+        return self.output_format.to_string(self.input_format.from_string(input_key))
+
+
+@dataclass
+class InputOutputFormatsPrivate:
+    input_format: type[PrivateKeyFormat]
+    output_format: type[PrivateKeyFormat]
+
+    def convert_key(self, input_key: str) -> str:
+        return self.output_format.to_string(self.input_format.from_string(input_key))
+
+
+def parse_args() -> Optional[Args]:
     arg_parser = ArgumentParser(
         description=(
             "Convert between RSA key formats. "
-            f"Supported private key formats: {' '.join(private_key_formats.keys())}. "
-            f"Supported public key formats: {' '.join(public_key_formats.keys())}."
+            f"Supported private key formats: {' '.join(PRIVATE_KEY_FORMATS.keys())}. "
+            f"Supported public key formats: {' '.join(PUBLIC_KEY_FORMATS.keys())}."
         )
     )
     arg_parser.add_argument(
@@ -49,11 +79,13 @@ def main():
         "-i",
         "--input_format",
         help="Key format for the input.",
+        required=True,
     )
     arg_parser.add_argument(
         "-o",
         "--output_format",
         help="Key format for the output.",
+        required=True,
     )
 
     args = arg_parser.parse_args()
@@ -62,7 +94,7 @@ def main():
         print("Unsupported mode:", args.mode)
         return
 
-    formats = private_key_formats if args.mode == "private" else public_key_formats
+    formats = PRIVATE_KEY_FORMATS if args.mode == "private" else PUBLIC_KEY_FORMATS
 
     if args.input_format not in formats.keys():
         print("Unsupported input format for mode", args.mode, ":", args.input_format)
@@ -74,11 +106,11 @@ def main():
         return
     output_format = formats[args.output_format]
 
-    input_key = args.input
-
-    try:
-        key = input_format.from_string(input_key)
-        print(output_format.to_string(key))
-    except ValueError as e:
-        print(str(e))
-        return
+    return Args(
+        input=args.input,
+        input_output_formats=(
+            InputOutputFormatsPrivate(input_format, output_format)  # type: ignore
+            if args.mode == "private"
+            else InputOutputFormatsPublic(input_format, output_format)  # type: ignore
+        ),
+    )
